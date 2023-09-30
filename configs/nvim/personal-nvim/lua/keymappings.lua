2a0514fa750874053930b0bda472ef4cfaf13059
Victor Simoes <victor.simoes@brmalls.com.br>
2023-09-30 12:08:58 -0300

    feat: adding LSP integration without lsp-zero

--- configs/nvim/personal-nvim/lua/keymappings.lua
+++ configs/nvim/personal-nvim/lua/keymappings.lua
@@ -1,82 +1,71 @@
 -- Your custom keybindings (replace with your own)
 vim.g.mapleader = " "
 
--- normal mode
-    -- Packer
-    vim.api.nvim_set_keymap("n", "<leader>ps", "<cmd>PackerSync<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>LspZeroFormat<CR>", { noremap = true, silent = true })
+local register_keymap = function(modes, cmd, mapping, opts)
+	for _, mode in ipairs(modes) do
+		if mode == "sv" then
+			mapping = "<Esc>" .. mapping
+			mode = "v"
+		end
+		vim.api.nvim_set_keymap(mode, cmd, mapping, opts)
+	end
+end
 
-vim.api.nvim_set_keymap("n", "<C-z>", "", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "q", "", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>RR", "<cmd>so $NVIM_PATH/init.lua<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>Q", ":qa!<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-c>", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-s>", "<cmd>w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>wa<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-d>", "<Esc>yyp", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>p", '"+p<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "q:", "", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-q>", "", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-t>", "<cmd>tabnew<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>1", "1gt", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>1", "1gt", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>2", "2gt", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>3", "3gt", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>4", "4gt", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>5", "5gt", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-f>", "za", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>LazyGit<CR>", { noremap = false, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>TransparentToggle<CR>", { noremap = false, silent = true })
-vim.api.nvim_set_keymap("n", "<C-n>", "<cmd>Neotree toggle<CR>", { noremap = false, silent = true })
+-- normal mode
+-- Packer
+
+register_keymap({ "n", "sv" }, "<leader>ps", "<cmd>PackerSync<CR>", { noremap = true, silent = true })
+-- register_keymap({ "n", "sv" }, "<leader>ff", ":lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
+register_keymap({ "n", "sv" }, "<leader>w", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
+
+register_keymap({ "n", "v" }, "<C-z>", "", { noremap = true, silent = true })
+register_keymap({ "n", "v" }, "q", "", { noremap = true, silent = true })
+register_keymap({ "n", "sv" }, "<leader>RR", "<cmd>so $NVIM_PATH/init.lua<CR>", { noremap = true, silent = true })
+register_keymap({ "n", "sv" }, "<leader>q", ":q<CR>", { noremap = true, silent = true })
+register_keymap({ "n", "sv" }, "<leader>Q", ":qa!<CR>", { noremap = true, silent = true })
+register_keymap({ "n", "sv" }, "<C-s>", "<cmd>w<CR>", { noremap = true, silent = true })
+register_keymap({ "n", "sv" }, "<leader>s", "<cmd>wa<CR>", { noremap = true, silent = true })
+register_keymap({ "n", "v" }, "<C-c>", "<Esc>", { noremap = true, silent = true })
+register_keymap({ "n" }, "<C-d>", "<Esc>yyp", { noremap = true, silent = true })
+register_keymap({ "n" }, "<leader>p", '"+p<CR>', { noremap = true, silent = true })
+register_keymap({ "n" }, "q:", "", { noremap = true, silent = true })
+register_keymap({ "n" }, "<C-q>", "", { noremap = true, silent = true })
+register_keymap({ "n" }, "<C-t>", "<cmd>tabnew<CR>", { noremap = true, silent = true })
+register_keymap({ "n" }, "<leader>1", "1gt", { noremap = true, silent = true })
+register_keymap({ "n" }, "<leader>1", "1gt", { noremap = true, silent = true })
+register_keymap({ "n" }, "<leader>2", "2gt", { noremap = true, silent = true })
+register_keymap({ "n" }, "<leader>3", "3gt", { noremap = true, silent = true })
+register_keymap({ "n" }, "<leader>4", "4gt", { noremap = true, silent = true })
+register_keymap({ "n" }, "<leader>5", "5gt", { noremap = true, silent = true })
+register_keymap({ "n" }, "<C-f>", "za", { noremap = true, silent = true })
+register_keymap({ "n" }, "<C-_>", "gcc", { noremap = false, silent = true })
+register_keymap({ "n", "sv" }, "<leader>gg", "<cmd>LazyGit<CR>", { noremap = false, silent = true })
+register_keymap({ "n" }, "<leader>tt", "<cmd>TransparentToggle<CR>", { noremap = false, silent = true })
+register_keymap({ "n" }, "<C-n>", "<cmd>Neotree toggle<CR>", { noremap = false, silent = true })
 
 -- visual mode
-vim.api.nvim_set_keymap("v", "<C-z>", "", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>y", '"+y<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-d>", "ykpgv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "p", "pgvyw", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>p", '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-_>", "gcgv", { noremap = false, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>Q", "<cmd>qa!<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>gg", "<Esc><cmd>LazyGit<CR>", { noremap = false, silent = true })
+register_keymap({ "v" }, "<leader>y", '"+y<CR>', { noremap = true, silent = true })
+register_keymap({ "v" }, "<C-d>", "ykpgv", { noremap = true, silent = true })
+register_keymap({ "v" }, "p", "pgvyw", { noremap = true, silent = true })
+register_keymap({ "v" }, "<leader>p", '"+p', { noremap = true, silent = true })
+register_keymap({ "v" }, "<C-_>", "gcgv", { noremap = false, silent = true })
 
-vim.api.nvim_set_keymap("x", "*", "/\\<<C-R>=expand('<cword>')<CR>\\><CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<", "<gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", ">", ">gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "K", ":move '<-2<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "J", ":move '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<leader>q", "<Esc>:q<CR>", { noremap = true, silent = true })
+register_keymap({ "x" }, "*", "/\\<<C-R>=expand('<cword>')<CR>\\><CR>", { noremap = true, silent = true })
+register_keymap({ "x" }, "<", "<gv", { noremap = true, silent = true })
+register_keymap({ "x" }, ">", ">gv", { noremap = true, silent = true })
+register_keymap({ "x" }, "K", ":move '<-2<CR>gv=gv", { noremap = true, silent = true })
+register_keymap({ "x" }, "J", ":move '>+1<CR>gv=gv", { noremap = true, silent = true })
 
 -- insert mode
-vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<Esc><leader>q", "<Esc>:q<CR>", { noremap = true, silent = true })
+register_keymap({ "i" }, "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
+register_keymap({ "i" }, "<Esc><leader>q", "<Esc>:q<CR>", { noremap = true, silent = true })
 
 -- Telescope
-vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-p>", "<Esc><cmd>Telescope find_files<CR>", { noremap = true, silent = true })
--- find files hidden too
-vim.api.nvim_set_keymap("n", "<C-P>", "<cmd>Telescope find_files hidden=true<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
-	"v",
-	"<C-P>",
-	"<Esc><cmd>Telescope find_files hidden=true<CR>",
-	{ noremap = true, silent = true }
-)
--- word grep
-vim.api.nvim_set_keymap("n", "<leader>w", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>w", "<Esc><cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap(
-	"n",
+register_keymap({ "n", "sv" }, "<C-p>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+register_keymap({ "n", "sv" }, "<C-P>", "<cmd>Telescope find_files hidden=true<CR>", { noremap = true, silent = true })
+register_keymap(
+	{ "n", "sv" },
 	"<leader>?",
 	"<cmd>Telescope current_buffer_fuzzy_find<CR>",
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"v",
-	"<leader>?",
-	"<Esc><cmd>Telescope current_buffer_fuzzy_find<CR>",
 	{ noremap = true, silent = true }
 )
--- configs/nvim/personal-nvim/lua/packerinit.lua
+++ configs/nvim/personal-nvim/lua/packerinit.lua
@@ -17,7 +17,71 @@ return require("packer").startup(function(use)
 	use(require("plugins.lazygit"))
 	use(require("plugins.comment"))
 	use(require("plugins.codeium"))
-	use(require("plugins.lsp"))
+	use("folke/neodev.nvim")
 
-	require("config.lsp-zero")
+	-- use(require("plugins.lsp"))
+
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
+
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			-- snippet engine
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+
+			-- autocompletion
+			"hrsh7th/cmp-nvim-lsp",
+
+			-- some snippets
+			"rafamadriz/friendly-snippets",
+		},
+	})
+
+	use({
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		requires = {
+			"williamboman/mason.nvim",
+			"jose-elias-alvarez/null-ls.nvim",
+		},
+
+		config = function()
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+								-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+								vim.lsp.buf.format({
+									async = true,
+									bufnr = bufnr,
+									filter = function(client)
+										return client.name == "null-ls"
+									end,
+								})
+							end,
+						})
+					end
+				end,
+			})
+			require("mason-null-ls").setup({
+				handlers = {},
+			})
+		end,
+	})
+
+	require("neodev").setup({})
+	require("config.lsp")
 end)
